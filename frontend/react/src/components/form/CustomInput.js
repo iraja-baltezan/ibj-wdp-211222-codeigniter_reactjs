abc989cce754807f09abc4420be08adc19d81b48
iraja-baltezan <iraja.baltezan@gmail.com>
2021-12-28 22:51:25 -0300

    factor: form component added

---
+++ frontend/react/src/components/form/CustomInput.js
@@ -0,0 +1,41 @@
+/**
+ * form
+ * This directory will hold custom form components.
+ *
+ * CustomInput.js
+ * This will create an input field of a specified type (text, number, email or
+ * password). If the input is invalid, an error message is displayed underneath.
+ */
+
+import React from 'react';
+import {Input, Label} from 'reactstrap';
+import {ErrorMessage, Field} from 'formik';
+
+const getErrorDiv = message => {
+    return (
+        <div style={{color: '#dc3545'}}>
+            {message}
+        </div>
+    );
+};
+const CustomInput = ({name, label, type = 'text'}) => {
+    return (<>
+        <Label for={name}>{label}</Label>
+        <Field name={name}>
+            {({field}) => {
+                return (<>
+                    <Input
+                        type={type}
+                        {...field}
+                        placeholder={label}/>
+                    <ErrorMessage
+                        name={name}
+                        render={getErrorDiv}
+                    />
+                </>)
+            }}
+        </Field>
+    </>);
+};
+
+export default CustomInput;
